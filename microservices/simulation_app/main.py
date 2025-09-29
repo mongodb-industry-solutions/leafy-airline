59c2aa6e7fe96f689fbbc972e850643d8f6b4abf
Patricia Renart <prenartcarnicero@gmail.com>
2025-09-29 10:41:45 +0200

    changes to allow new cloud deployment

--- microservices/simulation_app/main.py
+++ microservices/simulation_app/main.py
@@ -15,6 +15,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "https://airplanedashboard-65jcrv6puq-ew.a.run.app"
+    "https://airplanedashboard-test-502454695591.europe-west1.run.app"
+    # "*"
     # Add other origins if needed
 ]
 
