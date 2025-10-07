9edef60eac6544504be67e44a5ae42b11fc9b72f
Patricia Renart <prenartcarnicero@gmail.com>
2025-10-07 12:23:24 +0200

    Added conditional css for filters to make new approach more visual

--- airplanedashboard/components/TimeSlider.js
+++ airplanedashboard/components/TimeSlider.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './TimeSlider.module.css';
 
 const TimeSlider = ({ label, state, setter }) => {
-  
-  // Function to generate time values in 10-minute intervals
   const generateTimeValues = () => {
     let times = [];
     for (let h = 0; h < 24; h++) {
@@ -17,24 +15,59 @@ const TimeSlider = ({ label, state, setter }) => {
   };
 
   const timeValues = generateTimeValues();
+  const currentIndex = timeValues.indexOf(state);
+  const progress = (currentIndex / (timeValues.length - 1)) * 100;
 
   const handleSliderChange = (e) => {
     setter(timeValues[e.target.value]);
   };
 
-  return (
-    <div className={styles.filterTimeSlider}>
-      <div>{label}</div>
-      <input
-        type="range"
-        min="0"
-        max={timeValues.length - 1}
-        step="1"
-        value={timeValues.indexOf(state)}
-        onChange={handleSliderChange}
-        className={styles.slider}
-      />
-      <label>Selected Time: {state}</label>
+  return (<div>
+      {label === "Departure Time: " ? (
+        <div className={styles.filterTimeSlider}>
+        <div>{label}</div>
+        <input
+          type="range"
+          min="0"
+          max={timeValues.length - 1}
+          step="1"
+          value={currentIndex}
+          onChange={handleSliderChange}
+          className={styles.slider}
+          style={{
+            background: `linear-gradient(
+              to right,
+              #ddd ${progress}%,
+              #00684A ${progress}%
+            )`
+          }}
+        />
+        <label>Departing after: {state}</label>
+      </div>
+      ) : (
+        <div className={styles.filterTimeSlider}>
+        <div>{label}</div>
+        <input
+          type="range"
+          min="0"
+          max={timeValues.length - 1}
+          step="1"
+          value={currentIndex}
+          onChange={handleSliderChange}
+          className={styles.slider}
+
+          
+          style={{
+            background: `linear-gradient(
+              to right,
+              #00684A ${progress}%,
+              #ddd ${progress}%
+            )`
+          }}
+        />
+        <label>Arriving before: {state}</label>
+      </div>
+      )}
     </div>
   );
 };
